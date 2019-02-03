30b66e8fc536be75290670c125996f391e31c107
devaroop <>
2019-02-03 18:04:51 +0530

    add chart building template with jquery triggers for form date and current date

--- climate_change_app.py
+++ climate_change_app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_json import FlaskJSON, json_response
+
 app = Flask(__name__)
+FlaskJSON(app)
 
 @app.route('/')
 def dashboard():    
     return render_template('index.html')
+
+@app.route('/get_data')
+def get_data():
+    import pdb;pdb.set_trace()
+    given_date = request.args.get('date')
+    date, month, year = given_date.split('/')
+    result = fetch_data_for(day, month, year)
+    return json_response(result)
--- static/main.js
+++ static/main.js
@@ -1,11 +1,57 @@
-var tempChartCanvas = document.getElementById("tempChart").getContext('2d');
-var tempChart = new Chart(tempChartCanvas, {
-  type: 'line',
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-    }]
-  }
+var build_charts = function(data){
+  var tempChartCanvas = $("#tempChart").getContext('2d'),
+      humidityChart = $("#humidityChart").getContext('2d'),
+      vehicleChart = $("#vehicleChart").getContext('2d'),
+      treeChart = $("#treeChart").getContext('2d'),
+      populationChart = $("#populationChart").getContext('2d'),
+      waterChart = $("#waterChart").getContext('2d'),
+      ozoneChart = $("#ozoneChart").getContext('2d'),
+      noxChart = $("#noxChart").getContext('2d'),
+      so2Chart = $("#so2Chart").getContext('2d'),
+      pm25Chart = $("#pm25Chart").getContext('2d'),
+      pm10Chart = $("#pm10Chart").getContext('2d');
+
+  chart_template_build(tempChartCanvas, data["Year"], data["Temp(deg Centigrade)"], "Temp in deg C");
+  chart_template_build(humidityChart, data["Year"], data["% Humidity"], "% Humidity");
+  chart_template_build(vehicleChart, data["Year"], data["% Humidity"], "Total Vehicles in Pune");
+  chart_template_build(treeChart, data["Year"], data["% Humidity"], "Total Forest Cover");
+  chart_template_build(populationChart, data["Year"], data["% Humidity"], "Total Population in Pune");
+  chart_template_build(waterChart, data["Year"], data["% Humidity"], "Total ground water table");
+  chart_template_build(ozoneChart, data["Year"], data["% Humidity"], "Ozone Variation");
+  chart_template_build(noxChart, data["Year"], data["% Humidity"], "NOx variation");
+  chart_template_build(so2Chart, data["Year"], data["% Humidity"], "SO2 variation");
+  chart_template_build(pm25Chart, data["Year"], data["% Humidity"], "PM2.5 variation");
+  chart_template_build(pm10Chart, data["Year"], data["% Humidity"], "PM10 variation");
+};
+
+var chart_template_build = function(elem, xaxis, yaxis, label){
+  new Chart(elem, {
+    type: 'line',
+    data: {
+      labels: xaxis,
+      datasets: [{
+	label: label,
+	data: yaxis,
+      }]
+    }
+  });
+};
+
+$(document).ready(function(){
+  var date_query = $('#date').val();
+  query_for_data(date_query);
+
+  //set on change
+  $("#date").change(function(){
+    var date_query = $('#date').val();
+    query_for_data(date_query);
+  });
 });
+
+var query_for_data = function(date){
+  $.get("/get_data?date=" + date_query, function(data, status){
+    console.log(data);
+    build_charts(data);
+  });
+};
+
